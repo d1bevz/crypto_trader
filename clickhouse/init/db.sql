3e89a3aabe2c8713d1c7a0b3d0dc655b0dcf6153
d1bevz <d.p.bevz@gmail.com>
2023-01-03 23:10:00 +0300

    clickhouse configuration

---
+++ clickhouse/init/db.sql
@@ -0,0 +1,17 @@
+create table if not exists crypto_trade.candlesticks_history (
+    ticker String,
+    ts DateTime(),
+    open Float32,
+    high Float32,
+    low Float32,
+    close Float32,
+    vol Float32,
+    vol_ccy Float32,
+    updated DateTime() default now()
+)
+engine = MergeTree()
+order by (ticker, ts)
+partition by toDateTime(ts, 1)
+;
+
+drop database if exists default;
